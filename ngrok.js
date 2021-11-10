71b50b374b9e98b192989b7d67fd793e7329cce2
nightness <viipe.com@gmail.com>
2021-11-10 17:24:03 -0600

    building out cli arguments

--- ngrok.js
+++ ngrok.js
@@ -1,61 +1,74 @@
-const ngrok = require('ngrok');
-const fs = require('fs');
+const ngrok = require("ngrok");
+const fs = require("fs");
 const { exec } = require("child_process");
 
-const dotenvFile = '../.env'
+const dotenvFile = "../.env";
+
+const optionDefinitions = [
+  { name: "proto", type: String },
+  { name: "addr", type: Number },
+];
+
+const commandLineArgs = require("command-line-args");
+const options = commandLineArgs(optionDefinitions);
+
+console.log(options);
 
 // Read the .env file
-fs.readFile(dotenvFile, 'utf8', (err, data) => {
-	if (err) {
-		console.error(err);
-		return;
-	}
+fs.readFile(dotenvFile, "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-    // Convert string to string array, split at newlines
-    let lines = data.split('\n') // string[]
-    let token = null; // string || null
+  // Convert string to string array, split at newlines
+  let lines = data.split("\n"); // string[]
+  let authtoken = null; // string || null
 
-    // find the auth token
-    lines.forEach(element => {
-        const [name, value] = element.split('=');
-        if (name === 'NGROK_AUTHTOKEN')
-            token = value;
-    });
+  // find the auth token
+  lines.forEach((element) => {
+    const [name, value] = element.split("=");
+    if (name === "NGROK_AUTHTOKEN") authtoken = value;
+  });
 
-    // No token found
-    if (!token) {
-        console.error("Setup NGROK_AUTHTOKEN in your .env file")
-        return;
-    }
+  // No token found
+  if (!authtoken) {
+    console.error("Setup NGROK_AUTHTOKEN in your .env file");
+    return;
+  }
+
+  // Start ngrok
+  (async function () {
+    const proto = options?.proto ?? "http";
+    const addr = options?.addr ? options.addr : 3000;
 
     // Start ngrok
-	(async function () {
-		const url = await ngrok.connect({ authtoken: token, proto: 'http', addr: 3000 });
+    const url = await ngrok.connect({ authtoken, proto, addr });
 
-        // Rebuild lines with the new url
-        lines = lines.map(element => {
-            const [name] = element.split('=');
-            if (name === 'NGROK_SERVERHOST')
-                return `${name}=${url}`
-            return element;
-        })
+    // Rebuild lines with the new url
+    lines = lines.map((element) => {
+      const [name] = element.split("=");
+      if (name === "NGROK_SERVERHOST") return `${name}=${url}`;
+      return element;
+    });
 
-        // convert back to string format
-        let writeData = ''
-        lines.forEach(element => {
-            writeData += element + '\n'
-        })
-        writeData = writeData.slice(0, writeData.length - 1);
+    // convert back to string format
+    let writeData = "";
+    lines.forEach((element) => {
+      writeData += element + "\n";
+    });
+    writeData = writeData.slice(0, writeData.length - 1);
 
-        // Write the new .env file
-        fs.writeFile(dotenvFile, writeData, (err) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            console.clear();
-            console.log(`Your current ngrok url is ${url}.`);
-            console.log(`Your .env file has been updated.`);            
-          });		
-	})();
+    // Write the new .env file
+    fs.writeFile(dotenvFile, writeData, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.clear();
+      console.log(`Started ngrok: protocol '${proto}', addr '${addr}'`)
+      console.log(`Your current ngrok url is ${url}.`);
+      console.log(`Your .env file has been updated.`);
+    });
+  })();
 });
